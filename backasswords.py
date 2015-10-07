9a5fca806a7778c024f18267319602d4aadff2f7
Blake Klynsma <nodocify@gmail.com>
2015-10-06 19:30:48 -0600

    Remove remaining integers. Add license.

--- backasswords.py
+++ backasswords.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 
+"""
+Author: Nodocify
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
 def gen_num(s):
     codes = [ord(c) for c in s]
     return sum(codes[i] * 256 ** i for i in range(len(codes)))
 
 def gen_str(num):
-    #print(convert(num))
     return """(lambda _, __, ___, ____, _____, ______, _______, ________:
     getattr(
         __builtins__, (lambda _, __: _(_, __))(lambda _, __:
-            chr(__ %% (1 << 8)) + _(_, __ // (1 << 8)) if __ else (lambda: _).__code__.co_lnotab.decode(),
+            chr(__ %% ((lambda _: _).__code__.co_nlocals << (lambda _, __, ___, ____, _____, ______, _______, ________: _).__code__.co_nlocals)) + _(_, __ // ((lambda _: _).__code__.co_nlocals << (lambda _, __, ___, ____, _____, ______, _______, ________: _).__code__.co_nlocals)) if __ else (lambda: _).__code__.co_lnotab.decode(),
             (((___ << ___) + _) << ((((___ << __) + _) << _))) - (((_____ << ___) - _) << (((((_ << ___) + _)) << _))) + (___ << ((_ << ____) - _)) - (((_ << ____) - _) << _______) - (_______ << __) + _)
     )(
          (lambda _, __: _(_, __))(lambda _, __:
-            chr(__ %% (1 << 8)) + _(_, __ // (1 << 8)) if __ else (lambda: _).__code__.co_lnotab.decode(),
+            chr(__ %% ((lambda _: _).__code__.co_nlocals << (lambda _, __, ___, ____, _____, ______, _______, ________: _).__code__.co_nlocals)) + _(_, __ // ((lambda _: _).__code__.co_nlocals << (lambda _, __, ___, ____, _____, ______, _______, ________: _).__code__.co_nlocals)) if __ else (lambda: _).__code__.co_lnotab.decode(),
             %s)
     )
 )(
