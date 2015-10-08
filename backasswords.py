bad3c673f3cae1637981215e6b6fd27e5403d28b
Blake Klynsma <nodocify@gmail.com>
2015-10-08 00:19:01 -0600

    Add encrypt option. Add command line args. Add class object structure.

--- backasswords.py
+++ backasswords.py
@@ -14,12 +14,65 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
 
-def gen_num(s):
-    codes = [ord(c) for c in s]
-    return sum(codes[i] * 256 ** i for i in range(len(codes)))
+class backasswords(object):
+    def __init__(self, s, key_length=5, bit_shift=False, obfuscate=True, encrypt=True):
+        import os
+        self.s = s
+        self.key_length = key_length
+        self.bit_shift = bit_shift
+        self.encrypt = encrypt
+        self.obfuscate = obfuscate
+        self.check_opts()
 
-def gen_str(num):
-    return """(lambda _, __, ___, ____, _____, ______, _______, ________:
+    def check_opts(self):
+        if self.bit_shift:
+            self.obfuscate = True
+
+    def run(self):
+        if self.encrypt:
+            self.s = self.gen_brute_wrapper(self.gen_enc(self.s, self.key_length), self.key_length)
+        if self.obfuscate:
+            num = self.gen_num(self.s)
+            if self.bit_shift:
+                con = self.bitshift(num)
+                self.s = self.gen_str(con)
+            else:
+                self.s = self.gen_str(str(num))
+        return self.s
+
+    def gen_enc(self, s, key_length):
+        from itertools import cycle
+        import random
+        key = ''.join(str(random.randint(0,9)) for x in range(key_length))
+        data = bytearray(s.encode())
+        x = cycle(key)
+        for i in range(len(data)):
+            data[i] ^= int(next(x), 16)
+        return data
+
+    def gen_brute_wrapper(self, s, key_length):
+        return """____ = %s
+___ = getattr(__import__('itertools'), 'product')('0123456789',repeat=%d)
+while (lambda _: _).__code__.co_nlocals:
+    __ = getattr(__import__('itertools'), 'cycle')(
+        (lambda: _).__code__.co_lnotab.decode().join(next(___)))
+    _ = ____[:]
+    for _____ in range(len(_)):
+        _[_____] ^= int(next(__), True.__class__.__name__.__len__() ** (lambda __, ___: __).__code__.co_nlocals)
+    if ''.join([chr(_) for _ in [112,121,116,104,111,110]]) in _.decode():
+        try:
+            exec(_.decode())
+            break
+        except:
+            continue
+""" % (s, key_length)
+
+    def gen_num(self, s):
+        codes = [ord(c) for c in s]
+        return sum(codes[i] * 256 ** i for i in range(len(codes)))
+
+    def gen_str(self, num):
+        return """(lambda _, __, ___, ____, _____, ______, _______, ________:
     getattr(
         __builtins__, (lambda _, __: _(_, __))(lambda _, __:
             chr(__ %% ((lambda _: _).__code__.co_nlocals << (lambda _, __, ___, ____, _____, ______, _______, ________: _).__code__.co_nlocals)) + _(_, __ // ((lambda _: _).__code__.co_nlocals << (lambda _, __, ___, ____, _____, ______, _______, ________: _).__code__.co_nlocals)) if __ else (lambda: _).__code__.co_lnotab.decode(),
@@ -48,47 +101,76 @@ def gen_str(num):
         )
     )
 )
-""" % convert(num)
+""" % num
 
-def encode(num, depth):
-    if num == 0:
-        return "_ - _"
-    if num <= 8:
-        return "_" * num
-    return "(" + convert(num, depth + 1) + ")"
+    def encode(self, num, depth):
+        if num == 0:
+            return "_ - _"
+        if num <= 8:
+            return "_" * num
+        return "(" + self.bitshift(num, depth + 1) + ")"
 
-def convert(num, depth=0):
-    from math import ceil, log
-    result = ""
-    while num:
-        base = shift = 0
-        diff = num
-        span = int(ceil(log(abs(num), 1.5))) + (16 >> depth)
-        for test_base in range(span):
-            for test_shift in range(span):
-                test_diff = abs(num) - (test_base << test_shift)
-                if abs(test_diff) < abs(diff):
-                    diff = test_diff
-                    base = test_base
-                    shift = test_shift
-        if result:
-            result += " + " if num > 0 else " - "
-        elif num < 0:
-            base = -base
-        if shift == 0:
-            result += encode(base, depth)
-        else:
-            result += "(%s << %s)" % (encode(base, depth),
-                                      encode(shift, depth))
-        num = diff if num > 0 else -diff
-    return result
+    def bitshift(self, num, depth=0):
+        from math import ceil, log
+        result = ""
+        while num:
+            base = shift = 0
+            diff = num
+            span = int(ceil(log(abs(num), 1.5))) + (16 >> depth)
+            for test_base in range(span):
+                for test_shift in range(span):
+                    test_diff = abs(num) - (test_base << test_shift)
+                    if abs(test_diff) < abs(diff):
+                        diff = test_diff
+                        base = test_base
+                        shift = test_shift
+            if result:
+                result += " + " if num > 0 else " - "
+            elif num < 0:
+                base = -base
+            if shift == 0:
+                result += self.encode(base, depth)
+            else:
+                result += "(%s << %s)" % (self.encode(base, depth),
+                                          self.encode(shift, depth))
+            num = diff if num > 0 else -diff
+        return result
 
 if __name__ == '__main__':
-    import sys
-    try:
-        with open(sys.argv[1], 'r') as f:
-            data = f.read()
-        y = gen_str(gen_num(data))
-        print(y)
-    except:
-        print('Usage: python3 backasswords.py <script>')
+    import argparse
+    parser = argparse.ArgumentParser(description='Backasswords, an obfuscation script that will hide your plain text without changing the functionality.')
+    parser.add_argument('-o', '--obfuscate',
+                        action="store_true",
+                        default=False,
+                        help='Enable obfuscation')
+    parser.add_argument('-e', '--encrypt',
+                        action="store_true",
+                        default=False,
+                        help='Enable self-brute-forcing encryption')
+    parser.add_argument('-b', '--bitshift',
+                        action="store_true",
+                        default=False,
+                        help='Enable bitshift obfuscation, Implies obfuscation (WARNING: Takes a very long time to generate)')
+    parser.add_argument('-k', '--key',
+                        action="store",
+                        type=int,
+                        default=5,
+                        dest='length',
+                        help='Specify encryption key length, default = 5, Irrelevant if encryption is not in use.')
+    parser.add_argument('--output', action="store",
+                        default=None,
+                        dest='outfile',
+                        help='Specify output file. If not given writes to stdout.')
+    parser.add_argument('infile',
+                        action="store",
+                        help='Input python file to be obfuscated.')
+    r = parser.parse_args()
+    with open(r.infile, 'r') as f:
+        data = f.read()
+    y = backasswords(data, key_length=r.length, bit_shift=r.b, obfuscate=r.o, encrypt=r.e)
+    fin = y.run()
+    if r.outfile:
+        with open(r.outfile, 'w') as f:
+            f.write(fin)
+    else:
+        print(fin)
